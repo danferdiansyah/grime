08706fda71c27699de27bdc5e7dd1e0fd9325966
danferdiansyah <danferdianstyle@gmail.com>
2024-09-09 20:18:43 +0700

    Added name and class in views.py

--- main/views.py
+++ main/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render
 
 def show_main(request):
     context = {
-        'tagline': 'Anytime, anywhere.'
+        'tagline': 'Anytime, anywhere.',
+        'name' : 'Daniel Ferdiansyah',
+        'class' : 'PBP F'
     }
 
     return render(request, "main.html", context)

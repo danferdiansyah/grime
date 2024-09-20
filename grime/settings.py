b9145af7a4c7577234f5a93d62f607adeaf1baa2
danferdiansyah <danferdianstyle@gmail.com>
2024-09-21 00:36:41 +0700

    Created login form, auth, and connected models to User

--- grime/settings.py
+++ grime/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,7 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-5o7^2g8xz5sh=lwr_(8__iy2ub@4cqp@6#k++!%%95j&jm(bk5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+PRODUCTION = os.getenv("PRODUCTION", False)
+DEBUG = not PRODUCTION
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "daniel-ferdiansyah-grime.pbp.cs.ui.ac.id"]
 

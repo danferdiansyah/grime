552d5546619d75bd3ce2b8b80945d0abf0a9b82d
danferdiansyah <danferdianstyle@gmail.com>
2024-09-25 12:19:50 +0700

    Updated settings.py

--- grime/settings.py
+++ grime/settings.py
@@ -24,8 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-5o7^2g8xz5sh=lwr_(8__iy2ub@4cqp@6#k++!%%95j&jm(bk5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-PRODUCTION = os.getenv("PRODUCTION", False)
-DEBUG = not PRODUCTION
+DEBUG = True
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "daniel-ferdiansyah-grime.pbp.cs.ui.ac.id"]
 

f778dd6e894c14158d49a70631bfbed76634af27
danferdiansyah <danferdianstyle@gmail.com>
2024-10-02 23:08:20 +0700

    Implements AJAX on the product cards and added AJAX form

--- main/views.py
+++ main/views.py
@@ -10,17 +10,18 @@ from main.forms import ProductForm
 from main.models import Product
 from django.http import HttpResponse
 from django.core import serializers
+from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.http import require_POST
+from django.utils.html import strip_tags
 
 @login_required(login_url='/login')
 def show_main(request):
-    products = Product.objects.filter(user=request.user)
     context = {
         'tagline': 'anytime. anywhere.',
         'name' : 'Daniel Ferdiansyah',
         'class' : 'PBP F',
         'npm' : '2306275052',
         'login_user' : request.user.username,
-        'products' : products,
         'last_login': request.COOKIES['last_login'],
     }
 
@@ -44,11 +45,11 @@ def create_product(request):
     return render(request, "create_product.html", context)
     
 def show_xml(request):
-    data = Product.objects.all()
+    data = Product.objects.filter(user=request.user)
     return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
 
 def show_json(request):
-    data = Product.objects.all()
+    data = Product.objects.filter(user=request.user)
     return HttpResponse(serializers.serialize("json", data), content_type="application/json")
 
 def show_xml_by_id(request, id):
@@ -75,14 +76,16 @@ def register(request):
 
 def login_user(request):
    if request.method == 'POST':
-      form = AuthenticationForm(data=request.POST)
+    form = AuthenticationForm(data=request.POST)
 
-      if form.is_valid():
+    if form.is_valid():
         user = form.get_user()
         login(request, user)
         response = HttpResponseRedirect(reverse("main:show_main"))
         response.set_cookie('last_login', str(datetime.datetime.now()))
         return response
+    else:
+        messages.error(request, "Invalid username or password. Please try again.")
    else:
       form = AuthenticationForm(request)
    context = {'form': form,
@@ -114,3 +117,25 @@ def delete_product(request, id):
     product.delete()
 
     return HttpResponseRedirect(reverse('main:show_main'))
+
+@csrf_exempt
+@require_POST
+def add_product_ajax(request):
+    name = strip_tags(request.POST.get("name"))
+    price = request.POST.get("price")
+    description = strip_tags(request.POST.get("description"))
+    quantity = request.POST.get("quantity")
+    image = request.FILES.get("image")
+    user = request.user
+    
+    new_product = Product(
+        name=name, 
+        price=price,
+        description=description,
+        quantity=quantity,
+        image=image,
+        user=user
+    )
+    new_product.save()
+
+    return HttpResponse(b"CREATED", status=201)
